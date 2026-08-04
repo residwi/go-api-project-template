ea41cab100acd4fd23a413946238d04a3047f2a2
Resi Dwi <37761892+residwi@users.noreply.github.com>
2026-08-04 10:19:02 +0700

    refactor(logger): test the package from inside it

--- internal/platform/logger/logger_test.go
+++ internal/platform/logger/logger_test.go
@@ -1,37 +1,35 @@
-package logger_test
+package logger
 
 import (
 	"testing"
-
-	"github.com/residwi/go-api-project-template/internal/platform/logger"
 )
 
 func TestSetup(t *testing.T) {
 	t.Run("json format with info level", func(_ *testing.T) {
-		logger.Setup("info", "json")
+		Setup("info", "json")
 	})
 
 	t.Run("text format with debug level", func(_ *testing.T) {
-		logger.Setup("debug", "text")
+		Setup("debug", "text")
 	})
 
 	t.Run("warn level", func(_ *testing.T) {
-		logger.Setup("warn", "json")
+		Setup("warn", "json")
 	})
 
 	t.Run("warning level alias", func(_ *testing.T) {
-		logger.Setup("warning", "json")
+		Setup("warning", "json")
 	})
 
 	t.Run("error level", func(_ *testing.T) {
-		logger.Setup("error", "json")
+		Setup("error", "json")
 	})
 
 	t.Run("unknown level defaults to info", func(_ *testing.T) {
-		logger.Setup("unknown", "json")
+		Setup("unknown", "json")
 	})
 
 	t.Run("unknown format defaults to json", func(_ *testing.T) {
-		logger.Setup("info", "unknown")
+		Setup("info", "unknown")
 	})
 }
